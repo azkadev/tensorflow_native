7403c180b18ce258986eb23a4c5f5264341c367e
Your Name <you@example.com>
2022-07-11 14:56:46 +0700

    updae

---
+++ example/tensorflowlite_example.dart
@@ -0,0 +1,6 @@
+import 'package:tensorflowlite/tensorflowlite.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/tensorflowlite.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library tensorflowlite;
+
+export 'src/tensorflowlite_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
